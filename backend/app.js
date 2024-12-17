bf5e5c960f36606ffe9853a0919280edc97a3162
Raj <rajmalave4@gmail.com>
2024-12-17 11:31:30 +0530

    Update app.js CORS

--- backend/app.js
+++ backend/app.js
@@ -7,22 +7,30 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors(
-   {
-    origin: 'https://tripplanner-backend-qbqv.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,}
-));
+
+const allowedOrigins = ['https://tripplanner-c3ry.vercel.app', 'http://localhost:3000'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
-// Routes
+});
+
 app.use('/api/packages', require('./routes/packageRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
